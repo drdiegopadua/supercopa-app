a13e314030d25c0d0feefff6c3274450bfee892b
drdiegopadua <drdiegopadua@gmail.com>
2026-05-28 11:59:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
+const pages = document.querySelectorAll('.page');
+const navButtons = document.querySelectorAll('.nav-btn');
+
+function openTab(pageId, button = null){
+
+  pages.forEach(page => {
+    page.classList.remove('active-page');
+  });
+
+  document.getElementById(pageId).classList.add('active-page');
+
+  navButtons.forEach(btn => {
+    btn.classList.remove('active');
+  });
+
+  if(button){
+    button.classList.add('active');
+  }
+
+}
+
 const targetDate = new Date('2026-12-20T08:00:00');
 
 function updateCountdown(){
 
   const now = new Date();
-
   const diff = targetDate - now;
 
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
 
-  document.getElementById('countdown').innerHTML = `
-    ${days} DIAS ${hours}H
-  `;
+  document.getElementById('countdown').innerHTML = `${days} DIAS`;
 
 }
 
 updateCountdown();
 
 setInterval(updateCountdown, 1000);
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('sw.js');
-}
