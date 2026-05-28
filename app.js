93a4fca4846e3ab902c296dc3bb3828b4a2cee33
drdiegopadua <drdiegopadua@gmail.com>
2026-05-28 11:49:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,4 @@
-const themeToggle = document.getElementById('themeToggle');
-
-const savedTheme = localStorage.getItem('theme');
-
-if(savedTheme){
-  document.documentElement.setAttribute('data-theme', savedTheme);
-}
-
-themeToggle.addEventListener('click', () => {
-
-  const current = document.documentElement.getAttribute('data-theme');
-
-  const next = current === 'light'
-    ? 'dark'
-    : 'light';
-
-  document.documentElement.setAttribute('data-theme', next);
-
-  localStorage.setItem('theme', next);
-
-});
-
-const targetDate = new Date('2026-11-28T08:00:00');
+const targetDate = new Date('2026-12-20T08:00:00');
 
 function updateCountdown(){
 
@@ -29,9 +7,10 @@ function updateCountdown(){
   const diff = targetDate - now;
 
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
 
   document.getElementById('countdown').innerHTML = `
-    Faltam ${days} dias
+    ${days} DIAS ${hours}H
   `;
 
 }
