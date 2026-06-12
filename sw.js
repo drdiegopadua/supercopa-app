dc07d5a52a2da60ec6c1715cb57bd10b43663d4a
drdiegopadua <drdiegopadua@gmail.com>
2026-06-12 20:51:49 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,24 @@
-const CACHE_NAME = 'super-voleibol-copa-v1';
+const CACHE_NAME = 'supercopa-afc-v1';
 
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js'
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './pwa-icon-192.png',
+  './pwa-icon-512.png',
+  './apple-touch-icon.png'
 ];
 
 self.addEventListener('install', event => {
-
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
-
 });
 
 self.addEventListener('fetch', event => {
-
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
-
 });
